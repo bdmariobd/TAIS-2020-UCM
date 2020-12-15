bc1deca5dfabc583ed951ae5c8ed249f9e20a045
running004 <mario10_bd@hotmail.com>
2020-12-15 18:31:41 +0100

    RUN ERROR

--- 40/40/40.cpp
+++ 40/40/40.cpp
@@ -18,11 +18,44 @@
 
     palabra[i]=palabra[j] -> palindromo(i,j) = palindromo(i+1,j-1)
 
-    palabra[i]!=palabra[j] -> palindromo(i,j) = min (palidromo() , palindromo()  )
+    palabra[i]!=palabra[j] -> palindromo(i,j) = min (palidromo(i, j-1)+1 , palindromo(i+1, j)+1  )
 */
-int palindromo(const std::string& palabra, int i, int j, Matriz<int>& mtx) {
-
+void reconstruir(const std::string& palabra, int i, int j, const Matriz<int>& mtx, std::string&sol) {
+    //std::string sol="";
+    if (i > j) return;
+    if (i == j) sol.push_back(palabra[i]);
+    
+    else if (palabra[i] == palabra[j]) {
+        sol.push_back(palabra[i]);
+        reconstruir(palabra, i + 1, j - 1,  mtx,sol);
+        sol.push_back(palabra[j]);
+    }
+    else {
+        if (mtx[i][j] == mtx[i][j-1] + 1) { //quitar la j consigue un mejor resultado
+            sol.push_back(palabra[j]);
+            reconstruir(palabra, i , j - 1, mtx,sol);
+            sol.push_back(palabra[j]);
+        }
+        else { //quitar la i consigue un mejor resultado
+            sol.push_back(palabra[i]);
+            reconstruir(palabra, i+1, j, mtx,sol);
+            sol.push_back(palabra[i]);
+        }
+    }
+   
 }
+int palindromo(const std::string& palabra, int i, int j, Matriz<int>& mtx) {
+    if (mtx[i][j] != -1) return mtx[i][j];
+    else {
+        if (i == j) mtx[i][j] = 0;
+        else {
+            if (palabra[i] == palabra[j]) mtx[i][j] = palindromo(palabra, i + 1, j - 1, mtx);
+            else mtx[i][j] = std::min(palindromo(palabra, i, j - 1,mtx) + 1, palindromo(palabra, i + 1, j,mtx) + 1);
+        }
+        return mtx[i][j];
+    }
+}
+
 // resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
@@ -33,8 +66,12 @@ bool resuelveCaso() {
    if (!std::cin)  // fin de la entrada
       return false;
    int tam = palabra.size();
-   
-   
+   Matriz<int> mtx(tam, tam, -1);
+
+   int num = palindromo(palabra, 0, tam-1, mtx);
+   std::string sol = "";
+   reconstruir(palabra, 0, tam - 1, mtx, sol);
+   std::cout << num<<" "<<sol<<'\n';
    // escribir sol
    
    return true;
